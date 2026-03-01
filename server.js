68641d4cfef72c8005f393b365a7a1fcec53082e
Aadam Aftab <aadamaftab1126@gmail.com>
2026-03-01 16:49:01 +0530

    fix port for render

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ app.post("/api/data/:userId", (req, res) => {
   res.json({ status: "saved" });
 });
 
-app.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log("Server running on port", PORT)
 );
